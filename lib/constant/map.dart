b15372e8b98ae600ce35e520eff48c74e6816314
Call Vin <kanggara75@gmail.com>
2022-08-24 17:50:32 +0700

    add: mapbox service initial example

---
+++ lib/constant/map.dart
@@ -0,0 +1,9 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken =
+      'pk.eyJ1Ijoia2FuZ2dhcmE3NSIsImEiOiJjbDF0djJlNTcwNWp5M2twYW91OTN2eXdkIn0.jjhfriF72884yWDrx-mVXA';
+  static const String mapBoxStyleId = 'cl77g6mmc000714nzmh95zkv5';
+  static const String mapBoxUsername = 'kanggara75';
+  static final myLocation = LatLng(51.5090214, -0.1982948);
+}
